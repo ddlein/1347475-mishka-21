7213446e300d09a911a177ee08042271db47d97b
Дима <70834132+ddlein@users.noreply.github.com>
2021-01-18 21:21:51 +0300

    модалка

--- source/js/script.js
+++ source/js/script.js
@@ -16,27 +16,37 @@ navToggle.addEventListener('click', function () {
 // Popup
 
 let popup = document.querySelector('.popup');
+let popupWrapper = document.querySelector('.popup__wrapper')
 let popupButton = document.querySelector('.popup__button');
 let popupOpen = document.querySelectorAll('.popup-open');
 
 
 
-popupOpen.forEach ((element => {
-  element.addEventListener('click', () => {
-    popup.classList.add('popup--show')
-  })
-}))
+popupOpen.forEach(function (el) {
+  el.addEventListener("click", function (e) {
+    e.preventDefault();
+    popup.classList.remove("visually-hidden");
+    popup.classList.add("popup--show");
+    popupWrapper.classList.add("popup--open");
+    document.body.style.overflow = "hidden";
 
-// basketButton.addEventListener('click', () => {
-//   popup.classList.add('popup--show')
-// })
 
-popup.addEventListener('click', (evt) => {
-  if (evt.target.closest('.popup__wrapper') === null) {
-    popup.classList.remove('popup--show')
-  }
-})
+    if (popupWrapper.classList.contains("popup--open")) {
+      window.addEventListener("click", function (e) {
+        if (e.target === popup) {
+          document.body.style.overflow = "visible";
+          popup.classList.remove("popup--show");
+          popupWrapper.classList.remove("popup--open");
+        }
+      });
+      window.addEventListener("keydown", function (evt) {
+        if (evt.keyCode === 27) {
+          document.body.style.overflow = "visible";
+          popup.classList.remove("popup--show");
+          popupWrapper.classList.remove("popup--open");
+        }
+      });
 
-popupButton.addEventListener('click', () => {
-  popup.classList.remove('popup--show')
-})
+    }
+  });
+});
