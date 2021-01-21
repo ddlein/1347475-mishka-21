b51eef019a5c6f35cfadcab2f70bae6a2afc7762
Дима <70834132+ddlein@users.noreply.github.com>
2021-01-21 20:33:56 +0300

    Сделано все, кроме сборки

--- source/js/script.js
+++ source/js/script.js
@@ -1,7 +1,9 @@
 let navMain = document.querySelector('.main-nav');
 let navToggle = document.querySelector('.main-nav__toggle');
+let navClosed = document.querySelector('.main-nav--closed');
 
 navMain.classList.remove('main-nav--no-js');
+navMain.classList.add('main-nav--closed');
 
 navToggle.addEventListener('click', function () {
   if (navMain.classList.contains('main-nav--closed')) {
